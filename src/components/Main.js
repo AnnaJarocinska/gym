3d880dcf5279a5976a630984df5a44c6b6e751fc
Anna Jarocińska <anna.jarocinska5@gmail.com>
2019-08-29 13:09:31 +0200

    Remove Hashrouter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Hashrouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Offer from './Offer';
 import Schedule from './Schedule';
@@ -10,16 +10,14 @@ import Home from './Home';
 
 const Main = () => {
     return (
-        <Hashrouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/Offer" component={Offer} />
-                <Route path="/Schedule" component={Schedule} />
-                <Route path="/Description" component={Description} />
-                <Route path="/Trainers" component={Trainers} />
-                <Route path="/Contact" component={Contact} />
-            </Switch>
-        </Hashrouter>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/Offer" component={Offer} />
+            <Route path="/Schedule" component={Schedule} />
+            <Route path="/Description" component={Description} />
+            <Route path="/Trainers" component={Trainers} />
+            <Route path="/Contact" component={Contact} />
+        </Switch>
     );
 }
 
