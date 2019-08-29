6c431f67d6f36b6c5594ff672b5a40a4ce0d6674
Anna Jarocińska <anna.jarocinska5@gmail.com>
2019-08-29 16:21:17 +0200

    Replace Nav.Link with Link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Link } from 'react-bootstrap';
 import logo from '../Pictures/logo.png';
 import '../style/Navigation.css'
 
@@ -12,12 +12,18 @@ const Navigation = (props) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/" exact>O nas</Nav.Link>
+          {/* <Nav.Link href="/" exact>O nas</Nav.Link>
           <Nav.Link href="/Offer">Oferta</Nav.Link>
           <Nav.Link href="/Schedule">Grafik</Nav.Link>
           <Nav.Link href="/Description">Opis zajęć</Nav.Link>
           <Nav.Link href="/Trainers">Trenerzy</Nav.Link>
-          <Nav.Link href="/Contact">Kontakt</Nav.Link>
+          <Nav.Link href="/Contact">Kontakt</Nav.Link> */}
+          <Link to="/" exact>O nas</Link>
+          <Link to="/Offer">Oferta</Link>
+          <Link to="/Schedule">Grafik</Link>
+          <Link to="/Description">Opis zajęć</Link>
+          <Link to="/Trainers">Trenerzy</Link>
+          <Link to="/Contact">Kontakt</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
